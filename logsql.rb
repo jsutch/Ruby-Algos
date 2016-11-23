8e280cfc4d2f3941ea7331d9a12accd78948fb37
Jeff Sutch <jeff.sutch@acm.org>
2016-11-22 20:49:26 -0800

    scraping - round 2

--- logsql.rb
+++ logsql.rb
@@ -4,7 +4,7 @@ require 'mysql2'
 require 'json'
 
 begin
-	client = Mysql2::Client.new(:host => 'walrussy.net', :database => 'employees', :username => "jrat", :password => "WorldBookEncyclopedia")
+	client = Mysql2::Client.new(:host => 'walrussy.net', :database => 'employees', :username => "XXX", :password =>"XXX")
 
 #results = client.query("SELECT * FROM employees WHERE gender='f'")
 #results = client.query("SELECT * FROM employees WHERE gender='f' LIMIT 0, 10")
