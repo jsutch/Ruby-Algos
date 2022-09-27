5ddd7fa4114c62e626ffaa67351266d6ea146864
Jeff Sutch <jeff@collettpark.com>
2022-09-27 10:21:36 -0700

    small updates

---
+++ open.url.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#
+#
+#require "open-uri"
+#
+##remote_base_url = "http://en.wikipedia.org/wiki"
+#remote_base_url = "http://testsite.com"
+#remote_page_name = "bob"
+#remote_full_url = remote_base_url + "/" + remote_page_name
+#
+#remote_data = open(remote_full_url).read
+#my_local_file = open("my-downloaded-page.html", "w") 
+#
+#my_local_file.write(remote_data)
+#my_local_file.close
+#
